65568417bdc1550ed121595ec2e0c07a027a7619
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2022-08-25 14:05:50 +0530

    update readme and app.js for tutorial

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+  // -> 1. change here to return Hello World! instead of just Hello!
   return res.send('Hello!');
 });
 
+
+// -> 2. change 4567 to use a variable
 app.listen(4567, (err) => {
   if (err) {
     console.log(err);
