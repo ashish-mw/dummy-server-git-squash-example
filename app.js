9023b31b28259195e5c92313f9d084939133681a
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2022-08-25 14:31:37 +0530

    cleanup app.js
    - change to hello world!
    - change port to use a variable

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 4567;
+
 app.get('/', (req, res) => {
-  // -> 1. change here to return Hello World! instead of just Hello!
-  return res.send('Hello!');
+  return res.send('Hello World!');
 });
 
 
-// -> 2. change 4567 to use a variable
-app.listen(4567, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
     process.exit(1);
   }
-  console.log('Running on 4567');
+  console.log(`Running on ${PORT}`);
 });
 
 
